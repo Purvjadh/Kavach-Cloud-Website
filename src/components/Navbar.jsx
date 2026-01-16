53fb188bc15cc4a659656810df7a45d6182edbf4
AnushkaSinha4 <sinhaanushka444@gmail.com>
2026-01-16 16:32:09 +0530

    Initial project setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import '../styles/navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">Kavach Cloud</div>
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>Features</li>
+        <li>Login</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
